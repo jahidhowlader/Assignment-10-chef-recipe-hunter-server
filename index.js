d366792d469fbd8e66d4fdd7e21f693017cd9b33
Jahid Howlader <jahidhowlader.info@gmail.com>
2023-05-04 02:58:01 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+var cors = require('cors')
+
+const app = express()
+const PORT = 5000
+
+app.use(cors())
+
+const chefData = require('./data/chefdetails.json')
+
+app.get('/', (req, res) => {
+  res.send(chefData)
+})
+
+app.get('/chef/:_id', (req, res) => {
+    const id = +req.params._id
+    console.log(id);
+
+    const chefDetail = chefData.chefs.find(chef => +chef._id === id)
+    res.send(chefDetail)
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
